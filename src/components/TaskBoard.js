4f444e4bf8d061376463a44b227a7e2cc5261e92
Rrelle <rrelleferguson@usf.edu>
2026-04-12 17:41:00 -0400

    updated taskboard wit proper components

--- src/components/TaskBoard.js
+++ src/components/TaskBoard.js
@@ -6,13 +6,20 @@ import TaskList from './TaskList';
 import AddTaskForm from './AddTaskForm';
 import TaskStats from './TaskStats';
 
-export default function TaskBoard() {
+export default function TaskBoard() {()
+    const [tasks, setTasks] = useState([]);
+
+
+    function handleAdd(task) {
+        setTasks([...tasks,{id:crypto.randomUUID(),title:task,done:false}]);
+    }
+
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold">Task Manager</h1>
-            <AddTaskForm />
-            <TaskList />
-            <TaskStats />
+            <AddTaskForm onAdd={handleAdd} />
+            <TaskList tasks={tasks} />
+            <TaskStats tasks={tasks} />
         </div>
     );
 }
