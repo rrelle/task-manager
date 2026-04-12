1bdb3eab22db5485c67decad62f3e8ab4f1220b3
Rrelle <rrelleferguson@usf.edu>
2026-04-12 17:37:05 -0400

    updated addtask form w/functionality

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,3 +1,26 @@
-export default function AddTaskForm() {
-    return <div>Add Task Form</div>
+'use client'
+import { useState } from 'react';
+export default function AddTaskForm({ onAdd }) {
+    const [task, setTask] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (task.trim()) {
+            onAdd(task);
+            setTask('');
+        }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex gap-2">
+            <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Add a new task..."
+                className="border p-2 text-black"
+            />
+            <button type="submit" >Add</button>
+        
+        </form>
+    );
 }
