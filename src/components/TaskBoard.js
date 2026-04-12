4f666dbbb83d0e595bee2fddfd04933f047abb7e
Rrelle <rrelleferguson@usf.edu>
2026-04-12 17:05:40 -0400

    set up the taskboard main file

---
+++ src/components/TaskBoard.js
@@ -0,0 +1,20 @@
+'use client'
+
+import {useState, useEffect } from 'react';
+
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+import TaskStats from './TaskStats';
+
+export default function TaskBoard() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Task Manager</h1>
+            <AddTaskForm />
+            <TaskList />
+            <TaskStats />
+        </div>
+    );
+}
+
+    
